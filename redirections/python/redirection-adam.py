6e063a7dfaab51148dabe1c82d8e48d643ebf160
Adam Collado <datruth29@gmail.com>
2010-10-18 01:39:00 -0400

    Finished python redirection script

--- redirections/python/redirection-adam.py
+++ redirections/python/redirection-adam.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python
 
+import sys
+
+sout = sys.stdout
+#Move standout to file output.txt
+sys.stdout = open('output.txt', 'w')
+print "This is a file, receiving standard output."
+
+#Now were closing the file, and setting the standard out back to display
+sys.stdout.close
+sys.stdout = sout
+
+#Now, lets re-open the file, but with an Exception, to make sure it exists
+
+try:
+    thisfile = open('dutput.txt', 'r')
+except IOError:
+    print "This file does not exist, something went wrong."
+else:
+    print "The file was read correctly, here are the contents. :"
+    soutContent = thisfile.read()
+    print soutContent
+    thisfile.close
+
 #ValueError Exception
 while True:
     try:
